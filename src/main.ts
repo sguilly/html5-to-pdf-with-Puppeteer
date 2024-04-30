f3b8f94761a5dd1268dd7daff4838512b2ce9568
Guillaume GABOREL <guillaume.gaborel@s3pweb.com>
2024-04-30 10:18:24 +0200

    feat: add compression middleware

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { CorrelationIdMiddleware, HttpExceptionsLoggerFilter, LoggingService } from '@s3pweb/nestjs-common';
 import helmet from 'helmet';
+import { Constants } from './utils/constants.utils';
+import compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +14,14 @@ async function bootstrap() {
   // Add custom filter to log all http errors
   const { httpAdapter } = app.get(HttpAdapterHost);
   app.useGlobalFilters(new HttpExceptionsLoggerFilter(logger, httpAdapter));
+
+  if (Constants.convertConfigToBoolean(process.env.COMPRESSION)) {
+    app.use(compression());
+  }
+
+  // FOR DEBUG ONLY
+  // Logs all the mongoose queries to check filters
+  // mongoose.set('debug', true);
 
   // Helmet middleware
   app.use(helmet());
